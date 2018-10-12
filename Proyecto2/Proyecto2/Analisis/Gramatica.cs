954ca9252676b294774314d5013562cc12bd0ab4
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2018-10-11 19:24:51 -0600

    Condigo Fuente

---
+++ Proyecto2/Proyecto2/Analisis/Gramatica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Parsing;
+using Irony.Ast;
+
+namespace Proyecto2.Analisis
+{
+    class Gramatica : Grammar
+    {
+        public Gramatica() : base(caseSensitive: true)
+        {
+
+        } 
+    }
+}
